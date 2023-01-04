510f165f71ddbd57551e53b96b869445b17e8d40
ATAGA <abrahamt@veana.com>
2023-01-04 12:25:04 -0600

    1 rutas admin

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,19 @@ Auth::routes();
 
 Route::resource('tiendas', App\Http\Controllers\TiendaController::class)->middleware('auth');
 Route::resource('administrador', App\Http\Controllers\AdminController::class)->middleware('auth');
+
+Route::get('administrador/{id}/dam',[App\Http\Controllers\AdminController::class,'dam'])->name('administrdor.dam')->middleware('auth');
+
+Route::get('administrador/{admin}/tiendasid',[App\Http\Controllers\AdminController::class,'tiendasid'])->name('administrador.tiendasid')->middleware('auth');
+Route::get('administrador/{admin}/tiendasdamid',[App\Http\Controllers\AdminController::class,'tiendasdamid'])->name('administrador.tiendasdamid')->middleware('auth');
+Route::get('administrador/{admin}/diferenciasid',[App\Http\Controllers\AdminController::class,'diferenciasid'])->name('administrador.diferenciasid')->middleware('auth');
+Route::get('administrador/{admin}/diferencias',[App\Http\Controllers\AdminController::class,'diferencias'])->name('administrador.diferencias')->middleware('auth');
+Route::get('administrador/{admin}',[App\Http\Controllers\AdminController::class,'show'])->name('administrador.show')->middleware('auth');
+
+Route::get('administrador/{admin}/dam',[App\Http\Controllers\AdminController::class,'dam'])->name('administrador.dam')->middleware('auth');
+Route::get('tiendas/{id}/cruceprecios',[App\Http\Controllers\TiendaController::class,'cruceprecios'])->name('tiendas.cruceprecios')->middleware('auth');;
+Route::get('tiendas/{id}/detalle',[App\Http\Controllers\TiendaController::class,'detalle'])->name('tiendas.detalle')->middleware('auth');
+
 Route::get('tiendas/{id}/actualizar',[App\Http\Controllers\TiendaController::class,'actualizar'])->name('tiendas.actualizar');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
